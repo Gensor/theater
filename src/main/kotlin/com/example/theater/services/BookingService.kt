20f64bd842c68ec1e7cdf431d8ca1ee76dc5c756
Gensor <Gensor@users.noreply.github.com>
2022-03-08 20:49:07 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/theater/services/BookingService.kt
@@ -0,0 +1,12 @@
+package com.example.theater.services
+
+import com.example.theater.main.Seat
+import org.springframework.stereotype.Service
+
+@Service
+class BookingService {
+
+    fun isSeatFree(seat:Seat):Boolean = true
+
+
+}
